07a41a9ad9959be21f935a044eafd33650cd7b02
Tomoki Sugiura <sugiura@info.mie-u.ac.jp>
2026-03-06 23:03:27 +0900

    add testcase for compute_positive_series

--- model/mlp_in_keras/test_weight_to_resister.py
+++ model/mlp_in_keras/test_weight_to_resister.py
@@ -42,32 +42,65 @@ class TestWeightToRegister:
     def test_compute_one_neg_param(self):
         params_neg = np.array([-1.0])
         resisters, R = self.dut.compute_negative_series(params_neg)
-        assert np.allclose(resisters, np.array([-1000.0]))
+        assert np.allclose(resisters, np.array([-1_000.0]))
         assert np.isclose(R, 2.0)
 
         params_neg = np.array([-0.5])
         resisters, R = self.dut.compute_negative_series(params_neg)
-        assert np.allclose(resisters, np.array([-2000.0]))
+        assert np.allclose(resisters, np.array([-2_000.0]))
         assert np.isclose(R, 1.5)
 
     def test_compute_multiple_neg_params(self):
         params_neg = np.array([-1.0, -2.0])
         resisters, R = self.dut.compute_negative_series(params_neg)
-        assert np.allclose(resisters, np.array([-1000.0, -500.0]))
+        assert np.allclose(resisters, np.array([-1_000.0, -500.0]))
         assert np.isclose(R, 4.0)
 
         params_neg = np.array([-0.5, -0.25, -0.125])
         resisters, R = self.dut.compute_negative_series(params_neg)
-        assert np.allclose(resisters, np.array([-2000.0, -4000.0, -8000.0]))
+        assert np.allclose(resisters, np.array([-2_000.0, -4_000.0, -8_000.0]))
         assert np.isclose(R, 1.875)
 
     def test_compute_negative_with_zero(self):
         params_neg = np.array([-1.0, 0.0])
         resisters, R = self.dut.compute_negative_series(params_neg)
-        assert np.allclose(resisters, np.array([-1000.0, np.inf]))
+        assert np.allclose(resisters, np.array([-1_000.0, np.inf]))
         assert np.isclose(R, 2.0)
 
         params_neg = np.array([0.0, -0.5])
         resisters, R = self.dut.compute_negative_series(params_neg)
-        assert np.allclose(resisters, np.array([np.inf, -2000.0]))
+        assert np.allclose(resisters, np.array([np.inf, -2_000.0]))
         assert np.isclose(R, 1.5)
+
+    def test_compute_one_positive_param(self):
+        R = 2.0
+        pos_vals = np.array([1.0])
+        pos_series = self.dut.compute_positive_series(pos_vals, R)
+        assert np.allclose(pos_series, np.array([2_000.0]))
+
+        R = 1.5
+        pos_vals = np.array([0.5])
+        pos_series = self.dut.compute_positive_series(pos_vals, R)
+        assert np.allclose(pos_series, np.array([3_000.0]))
+
+    def test_compute_multiple_positive_params(self):
+        R = 4.0
+        pos_vals = np.array([1.0, 2.0])
+        pos_series = self.dut.compute_positive_series(pos_vals, R)
+        assert np.allclose(pos_series, np.array([4_000.0, 2_000.0]))
+
+        R = 1.875
+        pos_vals = np.array([0.5, 0.25, 0.125])
+        pos_series = self.dut.compute_positive_series(pos_vals, R)
+        assert np.allclose(pos_series, np.array([3_750.0, 7_500.0, 15_000.0]))
+
+    def test_compute_positive_with_zero(self):
+        R = 2.0
+        pos_vals = np.array([1.0, 0.0])
+        pos_series = self.dut.compute_positive_series(pos_vals, R)
+        assert np.allclose(pos_series, np.array([2_000.0, np.inf]))
+
+        R = 1.5
+        pos_vals = np.array([0.0, 0.1])
+        pos_series = self.dut.compute_positive_series(pos_vals, R)
+        assert np.allclose(pos_series, np.array([np.inf, 15_000.0]))
